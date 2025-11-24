30c75dc0de1191def37a65d6ff046bdedeeebf49
Roee Bar <75714824+Roee-Bar@users.noreply.github.com>
2025-11-24 22:34:21 +0200

    chore: clean up test output by suppressing act() warnings

--- jest.setup.js
+++ jest.setup.js
@@ -1,3 +1,17 @@
 // Learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom'
 
+// Suppress specific React warnings in tests to keep output clean
+const originalError = console.error;
+console.error = (...args) => {
+  // Suppress React act() warnings - these are common in async component tests
+  // and don't indicate actual test failures
+  if (
+    typeof args[0] === 'string' &&
+    args[0].includes('not wrapped in act')
+  ) {
+    return;
+  }
+  originalError.call(console, ...args);
+};
+
